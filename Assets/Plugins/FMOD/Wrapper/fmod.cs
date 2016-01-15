780468b0ac0c3eca21e287b3b69652afb4f90747
fmod-nick <nick@fmod.com>
2016-01-15 15:40:35 +1100

    Correct verion number in wrapper

--- Assets/Plugins/FMOD/Wrapper/fmod.cs
+++ Assets/Plugins/FMOD/Wrapper/fmod.cs
@@ -22,7 +22,7 @@ namespace FMOD
     */
     public class VERSION
     {
-        public const int    number = 0x00010700;
+        public const int    number = 0x00010705;
 #if UNITY_IPHONE && !UNITY_EDITOR
         public const string dll    = "__Internal";
 #elif (UNITY_PS4) && !UNITY_EDITOR
