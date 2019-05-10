b00ef4b16035863d287af613154fa1fb5f5fcc26
fmod-cameron <cameron@fmod.com>
2019-05-10 14:17:33 +1000

    Add missing files from 2.00.01

---
+++ Assets/Plugins/FMOD/src/Runtime/EventHandler.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+namespace FMODUnity
+{
+    public abstract class EventHandler : MonoBehaviour
+    {
+        public string CollisionTag = "";
+
+        void OnEnable()
+        {
+            HandleGameEvent(EmitterGameEvent.ObjectEnable);
+        }
+
+        void OnDisable()
+        {
+            HandleGameEvent(EmitterGameEvent.ObjectDisable);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (string.IsNullOrEmpty(CollisionTag) || other.CompareTag(CollisionTag))
+            {
+                HandleGameEvent(EmitterGameEvent.TriggerEnter);
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (string.IsNullOrEmpty(CollisionTag) || other.CompareTag(CollisionTag))
+            {
+                HandleGameEvent(EmitterGameEvent.TriggerExit);
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (string.IsNullOrEmpty(CollisionTag) || other.CompareTag(CollisionTag))
+            {
+                HandleGameEvent(EmitterGameEvent.TriggerEnter2D);
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            if (string.IsNullOrEmpty(CollisionTag) || other.CompareTag(CollisionTag))
+            {
+                HandleGameEvent(EmitterGameEvent.TriggerExit2D);
+            }
+        }
+
+        void OnCollisionEnter()
+        {
+            HandleGameEvent(EmitterGameEvent.CollisionEnter);
+        }
+
+        void OnCollisionExit()
+        {
+            HandleGameEvent(EmitterGameEvent.CollisionExit);
+        }
+
+        void OnCollisionEnter2D()
+        {
+            HandleGameEvent(EmitterGameEvent.CollisionEnter2D);
+        }
+
+        void OnCollisionExit2D()
+        {
+            HandleGameEvent(EmitterGameEvent.CollisionExit2D);
+        }
+
+        void OnMouseEnter()
+        {
+            HandleGameEvent(EmitterGameEvent.MouseEnter);
+        }
+
+        void OnMouseExit()
+        {
+            HandleGameEvent(EmitterGameEvent.MouseExit);
+        }
+
+        void OnMouseDown()
+        {
+            HandleGameEvent(EmitterGameEvent.MouseDown);
+        }
+
+        void OnMouseUp()
+        {
+            HandleGameEvent(EmitterGameEvent.MouseUp);
+        }
+
+        protected abstract void HandleGameEvent(EmitterGameEvent gameEvent);
+    }
+}
