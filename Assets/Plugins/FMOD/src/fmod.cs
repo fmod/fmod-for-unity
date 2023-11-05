1fbde772ca4874511cfe6bf398e9efc2f698f961
Andrew Hockey <andrew@fmod.com>
2023-11-06 10:58:17 +1100

    Update for 2.02.19

--- Assets/Plugins/FMOD/src/fmod.cs
+++ Assets/Plugins/FMOD/src/fmod.cs
@@ -19,7 +19,7 @@ namespace FMOD
     */
     public partial class VERSION
     {
-        public const int    number = 0x00020218;
+        public const int    number = 0x00020219;
 #if !UNITY_2019_4_OR_NEWER
         public const string dll    = "fmod";
 #endif
@@ -194,6 +194,7 @@ namespace FMOD
         AAUDIO,
         AUDIOWORKLET,
         PHASE,
+        OHAUDIO,
 
         MAX,
     }
