d9e969546b92e784f005d2c5a6025b1dd40d00ae
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2026-03-14 11:30:02 +0100

    use TryGetComponent instead of using GetComponent twice.

--- Assets/Plugins/FMOD/src/StudioEventEmitter.cs
+++ Assets/Plugins/FMOD/src/StudioEventEmitter.cs
@@ -275,18 +275,16 @@ namespace FMODUnity
                 if (is3D)
                 {
 #if UNITY_PHYSICS_EXIST
-                    if (GetComponent<Rigidbody>())
+                    if (TryGetComponent(out Rigidbody rigidBody))
                     {
-                        Rigidbody rigidBody = GetComponent<Rigidbody>();
                         instance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject, rigidBody));
                         RuntimeManager.AttachInstanceToGameObject(instance, gameObject, rigidBody);
                     }
                     else
 #endif
 #if UNITY_PHYSICS2D_EXIST
-                    if (GetComponent<Rigidbody2D>())
+                    if (TryGetComponent(out Rigidbody2D rigidBody2D))
                     {
-                        var rigidBody2D = GetComponent<Rigidbody2D>();
                         instance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject, rigidBody2D));
                         RuntimeManager.AttachInstanceToGameObject(instance, gameObject, rigidBody2D);
                     }
